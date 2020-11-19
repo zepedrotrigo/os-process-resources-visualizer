542abbcc326a3d951d867e508058c5c1c774f848
José Trigo <josetrigo@ua.pt>
2020-11-19 12:28:39 +0000

    19nov

--- procstat.sh
+++ procstat.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-# apresenta estatísticas sobre a memória usada por processos e sobre a quantidade de I/O 
-# que uma selecção de processos estão a efetuar
-
-# visualizar a quantidade de memória total de um processo, a quantidade de memória física 
-# ocupada por um processo, o número total e bytes de I/O que umprocesso leu/escreveu 
-# e também a taxa de leitura/escrita correspondente aos últimos s segundos para de um processo 
-# (o valor de s é passado como parametro).
-
-# permite a visualização da quantidade de memória total e da memória
-# residente em memória física (linhas VmSize e VmRSS de /proc/[pid]/status), do número de
-# total de bytes de I/O (linhas rchar e wchar de /proc/[pid]/io) e da taxa de leitura/escrita
-# (em bytes por segundo) dos processos seleccionados nos últimos s segundos (calculadas a partir de 2
-# leituras de /proc/[pid]/io com intervalo de s segundos).
-# parâmetro obrigatório que é o número de segundos que serão usados para calcular as taxas de I/O
-
 # awk, bc, cat, cut, date, getopts, grep, head, ls, printf, sleep, sort
 
 cd /proc # Mudar a diretoria para /proc
+printf '%s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\n' "COMM" "USER" "PID" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE" # Cabeçalho da tabela
 
 for entry in /proc/*; do # ciclo for para cada ficheiro ou diretoria contido em /proc/
-    entry_basename="$(basename $entry)" # obter apenas o basename (caminho relativo da pasta) 
+    entry_basename="$(basename $entry)" # obter apenas o basename (caminho relativo da pasta) (o PID)
     if [[ $entry_basename =~ ^[0-9]+$ ]]; then # Obter apenas folders ou files com nomes apenas númericos
+        comm=$(cat $entry_basename/comm)
+        user="$( ps -o uname= -p "${entry_basename}" )"
 
-        VmSize=$(grep 'VmSize' $entry_basename/status)
-        Vmsize_final=$(cut -d " " $VmSize) # Ficamos aqui, precisamos de obter apenas o numero.
-        echo $Vmsize_final
+        VmSize=$(grep 'VmSize' $entry_basename/status) # Obter todas as linhas com VmSize
+        VmSize_value=$(echo $VmSize | grep -o -E '[0-9]+') # Obter apenas o valor numérico
         VmRSS=$(grep 'VmRSS' $entry_basename/status)
+        VmRSS_value=$(echo $VmRSS | grep -o -E '[0-9]+')
         rchar=$(grep 'rchar' $entry_basename/io)
+        rchar_value=$(echo $rchar | grep -o -E '[0-9]+')
         wchar=$(grep 'wchar' $entry_basename/io)
-        #printf '%s %s \n' "${VmSize[@]}" "${VmRSS[@]}"
-        #printf '%s\n' "${VmSize[@]}"
+        wchar_value=$(echo $wchar | grep -o -E '[0-9]+')
+
+        if [[ $VmSize_value == "" ]]; then
+            VmSize_value="0"
+        fi
+
+        if [[ $VmRSS_value == "" ]]; then
+            VmRSS_value="0"
+        fi 
+
+        printf '%s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\t %s\n' "$comm" "$user" "$entry_basename" "$VmSize_value" "$VmRSS_value" "$rchar_value" "$wchar_value" "placeholder" "placeholder" "placeholder"
     fi
 done
+
+
+# rchar vai ser o readb, wchar -> writeb, e o rater e ratew são a taxa de leitura/escrita em bytes por segundo dos processos
