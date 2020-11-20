5662afdc386bc5c802498f4269f04899a3969d17
José Trigo <josetrigo@ua.pt>
2020-11-20 14:19:18 +0000

    fixed formatting

--- procstat.sh
+++ procstat.sh
@@ -2,7 +2,7 @@
 # awk, bc, cat, cut, date, getopts, grep, head, ls, printf, sleep, sort
 
 cd /proc # Mudar a diretoria para /proc
-printf '%-20s\t %-10s\t %10s\t %10s\t %10s\t %10s\t %10s\t %3s\t %6s\t %5s\n' "COMM" "USER" "PID" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE" # Cabeçalho da tabela
+printf '%-20s\t\t %-10s\t\t %10s\t %10s\t %10s\t %10s\t %9s\t %3s\t %6s\t %5s\n' "COMM" "USER" "PID" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE" # Cabeçalho da tabela
 
 for entry in /proc/*; do # ciclo for para cada ficheiro ou diretoria contido em /proc/
     entry_basename="$(basename $entry)" # obter apenas o basename (caminho relativo da pasta) (o PID)
@@ -20,11 +20,11 @@ for entry in /proc/*; do # ciclo for para cada ficheiro ou diretoria contido em 
         wchar_value=$(echo $wchar | grep -o -E '[0-9]+')
 
         if [[ $VmSize_value == "" ]]; then
-            VmSize_value="0"
+            VmSize_value="N/A"
         fi
 
         if [[ $VmRSS_value == "" ]]; then
-            VmRSS_value="0"
+            VmRSS_value="N/A"
         fi 
 
         #if [[ ${#user} -gt 15 ]]; then
@@ -32,7 +32,7 @@ for entry in /proc/*; do # ciclo for para cada ficheiro ou diretoria contido em 
         #fi
         #https://qastack.com.br/unix/396223/bash-shell-script-output-alignment
 
-        printf '%-20s\t %-10s\t %10s\t %10s\t %10s\t %10s\t %9s\t %5s\t %6s\t %5s\n' "$comm" "$user" "$entry_basename" "$VmSize_value" "$VmRSS_value" "$rchar_value" "$wchar_value" "****" "****" "****"
+        printf '%-30s\t %-20s\t %10s\t %10s\t %10s\t %10s\t %9s\t %5s\t %6s\t %5s\n' "$comm" "$user" "$entry_basename" "$VmSize_value" "$VmRSS_value" "$rchar_value" "$wchar_value" "****" "****" "****"
     fi
 done
 
